7abafc3d7c5f1666576858994ef2d948005ed37f
DeepakshiPurushotham <107757971+DeepakshiPurushotham@users.noreply.github.com>
2022-11-22 09:04:26 +0530

    Update variables.tf

--- modules/aws/vpc/variables.tf
+++ modules/aws/vpc/variables.tf
@@ -4,32 +4,21 @@ variable "vpn_range" {
 }
 variable "vpc_cidr_block" {
     type = string
-    #default = "10.0.0.0/16"
 }
 variable "publicSubnets1_cidr_block" {
     type = string
-    #default = "10.0.10.0/24"
 }
 variable "publicSubnets2_cidr_block" {
     type = string
-    #default = "10.0.20.0/24"
 }
 variable "privateSubnets1_cidr_block" {
     type = string
-    #default = "10.0.30.0/24"
 }
 variable "privateSubnets2_cidr_block" {
     type = string
-    #default = "10.0.40.0/24"
 }
-/*variable "EIP_Count" {
-    type = number
-    default = 1
-}*/
-
 variable "environment" {
     type = string
-    #default = "testing"
 }
 locals {
     common_tags = {
